61ebfd142720b24cda6feb4098d3ae831bff1f5a
Hilary Lyttle <hilyttle@gmail.com>
2016-12-20 19:33:05 +0000

    Adjusting nav and adding to teams controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,15 +2,27 @@ class TeamsController < ApplicationController
   before_action :set_team
 
   def index
+    @teams = Team.all
   end
 
   def show
+    unless authorize team
+      redirect_to teams_path
+    end
   end
 
   def new
+    @team = Team.new
   end
 
   def create
+    team = Team.new(team_params)
+    if authorize team
+      team.save
+      redirect_to team_path(team)
+    else
+      redirect_to new_team_path
+    end
   end
 
   def edit
