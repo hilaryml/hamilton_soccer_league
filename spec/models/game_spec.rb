763f6736a154b2cbac832e96b8753bbe5ac0076b
Hilary Lyttle <hilyttle@gmail.com>
2016-12-20 20:01:48 +0000

    Updating game model spec

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -4,10 +4,11 @@ RSpec.describe Game, :type => :model do
   let(:game) {
     Game.create(
       :datetime => "#{Time.now.strftime("%Y-%m-%d %H:%M:%S")}"
+      :location => "City Park"
     )
   }
 
-  it "is valid with a datetime" do
+  it "is valid with a datetime and a location" do
     expect(game).to be_valid
   end
 
