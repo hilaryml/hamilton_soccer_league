f581ed13d353c75d9fa6c47e45da650775b21419
Hilary Lyttle <hilyttle@gmail.com>
2016-12-23 19:54:52 +0000

    Add custom attribute writers to team model

--- app/models/team.rb
+++ app/models/team.rb
@@ -7,6 +7,19 @@ class Team < ActiveRecord::Base
 
   validates :name, presence: true
 
+  def coach_attributes=(coach)
+		team_coach = User.find_or_create_by(email: coach.email)
+		team_coach.update(coach)
+		team_coach.update(role: 1)
+		self.users << team_coach
+	end
+
+	def player_attributes=(player)
+		team_player = User.find_or_create_by(email: player.email)
+		team_player.update(player)
+		self.users << team_player
+	end
+
   def players
     self.users.where(role: 0)
   end
