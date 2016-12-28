43a6e5dc79892a1cf2bc50862e9f551ef6a17991
Hilary Lyttle <hilyttle@gmail.com>
2016-12-28 23:16:18 +0000

    Fixes omniauthable

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,10 @@ class User < ActiveRecord::Base
   belongs_to :team
 
   def self.from_omniauth(auth)
-  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-    user.email = auth.info.email
-    user.password = Devise.friendly_token[0,20]
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
   end
 
 end
