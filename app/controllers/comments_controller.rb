5c2ec4cddff862a6ee2728ccd53cba51bc06ab44
Hilary Lyttle <hilyttle@gmail.com>
2016-12-30 20:03:35 +0000

    Adds comment partials and renders them in game show view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,23 @@
 class CommentsController < ApplicationController
 
   def new
-    @comment = Comment.new
+    #@comment = Comment.new
   end
 
   def create
+    @comment = Comment.new(comment_params)
+    if @comment.valid?
+      @comment.save
+      redirect_to team_game_path(current_user.team, @comment.game)
+    else
+      redirect_to teams_path, alert: "Unsuccessful. Please, try again."
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
   end
 
 end
