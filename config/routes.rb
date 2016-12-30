1453e35ba1e6dfd9551e3670ca50785e10a14ba0
Hilary Lyttle <hilyttle@gmail.com>
2016-12-30 18:47:26 +0000

    Adds comment controller and new comment link to game show view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks", registrations: 'users/registrations' }
 
+  resources :comments
 
   resources :teams do
     resources :users
