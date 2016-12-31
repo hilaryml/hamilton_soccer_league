62e69c6b8971d4e71433a5cafcd6ed99124ef175
Hilary Lyttle <hilyttle@gmail.com>
2016-12-31 03:19:32 +0000

    Fixes minor error in games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -13,7 +13,7 @@ class GamesController < ApplicationController
     if @game.valid?
       if authorize @game
         current_user.team.games << @game
-        game.save
+        @game.save
         redirect_to team_path(current_user.team), alert: "Game successfully created."
       else
         redirect_to team_path(current_user.team), alert: "You are not authorized to complete this action."
