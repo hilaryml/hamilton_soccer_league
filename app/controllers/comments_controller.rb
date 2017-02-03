e1db1d1682d117f5958ab29e51b3a4a3fc23172d
hilaryml <hilyttle@gmail.com>
2017-02-02 19:59:22 -0500

    Fixes bug in js comment creation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,15 +4,8 @@ class CommentsController < ApplicationController
   end
 
   def create
-    @comment = Comment.new(comment_params)
-    if @comment.valid?
-      @comment.save
-      respond_to do |format|
-        format.json { render json: @comment, status: 201 }
-      end
-    else
-      redirect_to teams_path, alert: "Unsuccessful. Please, try again."
-    end
+    @comment = Comment.create(comment_params)
+    render json: @comment, status: 201
   end
 
   private
